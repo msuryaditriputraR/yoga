4519b380c69a6ca6f47b0819643e956a28406bf2
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-03-20 20:40:20 +0800

    add:  show / hidden menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,38 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
 
+// MENU SHOW
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    });
+}
+
+// MENU HIDDEN
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLinks = document.querySelectorAll(".nav__link");
 
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show-menu");
+};
+
+navLinks.forEach((link) => link.addEventListener("click", linkAction));
 
 /*=============== ADD BLUR TO HEADER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
