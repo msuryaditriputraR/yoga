acb5e3e4d91d2b5d62dfb39afb86748cd18ac747
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-03-22 17:15:57 +0800

    feat: dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,5 +70,40 @@ const scrollActive = () => {
 window.addEventListener("scroll", scrollActive);
 
 /*=============== DARK LIGHT THEME ===============*/
+const themeToggle = document.getElementById("theme-toggle");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+// get previous theme selected
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// validate if user selected theme previously
+if (selectedTheme) {
+    // change the theme according to the previous user's choice
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+        darkTheme
+    );
+    themeToggle.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+        iconTheme
+    );
+}
+
+// create function to get current theme to save in local storage
+const getCurrentTheme = () =>
+    document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+    themeToggle.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+// Activate / deactivate the theme with the button
+themeToggle.addEventListener("click", () => {
+    // ADD or remove dark class
+    document.body.classList.toggle(darkTheme);
+    themeToggle.classList.toggle(iconTheme);
+
+    // save to localstorage what theme user choose
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
